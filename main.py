59bf2949c994b6b192043ec26ee1236538a4d024
Zeragorn <rosokin52@gmail.com>
2024-11-15 18:56:34 +0300

    beta 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-BOT_VERSION = "Beta 1.1.1"
+BOT_VERSION = "Beta 3"
 
 #Проверка наличия конфига
 
@@ -7,12 +7,14 @@ try:
         print("Config status: found ")
 except FileNotFoundError:
     with open("config.py","w+") as config:
-        config.write(f"BOT_API=\"{input("Введите api бота: ")}\"")
+        config.write(f"BOT_API=\"{input("Enter bot api: ")}\"\nlang=\"{input("select lang(en/ru): ")}\"")
+
+
 
 #импорт библиотек
 import telebot
 import asyncio
-
+import json
 import requests
 from config import *
 from telebot import types
@@ -20,36 +22,52 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 bot = telebot.TeleBot(BOT_API)
 
+def load_translation(lang):
+    try:
+        with open(f"./lang/{lang}.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Ошибка: файл перевода для языка {lang} не найден.")
+        return {}
+
+def tr(key):
+    translations = load_translation(lang)
+    return translations.get(key, key)
+
 @bot.message_handler(commands=['start'])
 def start(message):
     with open('./test.png', 'rb') as img:
-        text = "Добро пожаловать в нашего телеграмм бота для оплаты ЖКХ \nЭтот бот поможет вам:\n · ййцц"
-        markup = types.InlineKeyboardMarkup()
-        markup.row(InlineKeyboardButton(text="Button test", callback_data="start_but"),InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.add(InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.add(InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.row(InlineKeyboardButton(text="inf", callback_data="inf"),InlineKeyboardButton(text="Профиль", callback_data="Profile"),InlineKeyboardButton(text="Поддержка", callback_data="pod"))
+        text = tr("main_tx")
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text=tr("caog_bt"), callback_data="catalog"))
+        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),InlineKeyboardButton(text=tr("prof_bt"), callback_data="Profile"),InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
 
         bot.send_photo(message.chat.id, img, caption=text, reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data == "inf")
 def inf(call):
     with open("./test_2.png","rb") as img:
-        markup = types.InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(text="<- назад",callback_data="main_menu"))
-        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=f"Ваш id: {call.message.chat.id}"),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=markup)
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text=tr("back_bt"),callback_data="main_menu"))
+        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=f"Bot version: {BOT_VERSION}\nMS_ID: {call.message.message_id}"),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data == "main_menu")
 def main_menu(call):
     with open('./test.png', 'rb') as img:
-        text = "Добро пожаловать в нашего телеграмм бота для оплаты ЖКХ \nЭтот бот поможет вам:\n · ййцц"
-        markup = types.InlineKeyboardMarkup()
-        markup.row(InlineKeyboardButton(text="Button test", callback_data="start_but"),InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.add(InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.add(InlineKeyboardButton(text="Button test", callback_data="start_but"))
-        markup.row(InlineKeyboardButton(text="inf", callback_data="inf"),InlineKeyboardButton(text="Профиль", callback_data="Profile"),InlineKeyboardButton(text="Поддержка", callback_data="pod"))
+        text = tr("main_tx")
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text=tr("caog_bt"), callback_data="catalog"))
+        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),InlineKeyboardButton(text=tr("prof_bt"), callback_data="profile"),InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
 
         bot.edit_message_media(types.InputMediaPhoto(media=img, caption=f"Добро пожаловать в нашего телеграмм бота для оплаты ЖКХ \nЭтот бот поможет вам:\n · ййцц"), reply_markup=markup,chat_id=call.message.chat.id,message_id=call.message.message_id)
+
+@bot.callback_query_handler(func=lambda call: call.data == "support")
+def inf(call):
+    with open("./test_2.png","rb") as img:
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text=tr("back_bt"),callback_data="main_menu"))
+        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=tr("supp_tx")),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=markup)
+
 
 #Контроль через консоль
 async def console_control():
