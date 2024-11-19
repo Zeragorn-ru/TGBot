e2ab1b84180a78af26e82d4c05bac4fb62ad7ff5
Zeragorn <rosokin52@gmail.com>
2024-11-19 20:58:08 +0300

    beta 4.5a

---
+++ lang_file.py
@@ -0,0 +1,25 @@
+
+ru = {
+"main_tx": "Добро пожаловать в нашего телеграмм бота для оплаты ЖКХ \nЭтот бот поможет вам:\n · ййцц {BOT_VERSION}",
+"info_bt": "Информация",
+"prof_bt": "Профиль",
+"supp_bt": "Поддержка",
+"caog_bt": "Каталог",
+"back_bt": "<-Назад",
+"supp_tx": "Поддержка: @Zeragorn\nРабочие часы:\nПн-Пт: 14:00 - 20:00\nСб-Вс: Выходной",
+"prof_tx": "Поддержка: @Zeragorn\nРабочие часы:\nПн-Пт: 14:00 - 20:00\nСб-Вс: Выходной"
+}
+
+en = {
+    "main_tx": "Welcome to our Telegram bot for paying utilities\nThis bot will help you:\n · {BOT_VERSION}",
+    "info_bt": "Information",
+    "prof_bt": "Profile",
+    "supp_bt": "Support",
+    "caog_bt": "Catalog",
+    "back_bt": "<-Back",
+    "supp_tx": "Support: @Zeragorn\nWorking hours:\nMon-Fri: 14:00 - 20:00\nSat-Sun: Day off",
+    "prof_tx": "Support: @Zeragorn\nWorking hours:\nMon-Fri: 14:00 - 20:00\nSat-Sun: Day off"
+}
+
+
+lang_list = {"ru": ru, "en": en}
--- main.py
+++ main.py
@@ -1,38 +1,38 @@
+# -*- coding: windows-1251 -*-
 BOT_VERSION = "Beta 3a"
 
-#РџСЂРѕРІРµСЂРєР° РЅР°Р»РёС‡РёСЏ РєРѕРЅС„РёРіР°
+#Проверка наличия конфига
 
 try:
-    with open("config.py","r"):
+    with open("config.py", "r"):
         print("Config status: found ")
 except FileNotFoundError:
-    with open("config.py","w+") as config:
+    with open("config.py", "w+") as config:
         config.write(f"BOT_API=\"{input("Enter bot api: ")}\"\nlang=\"{input("select lang(en/ru): ")}\"")
 
-
-
-#РёРјРїРѕСЂС‚ Р±РёР±Р»РёРѕС‚РµРє
+#импорт библиотек
 import telebot
 import asyncio
-import json
 import requests
 from config import *
+from lang_file import *
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 bot = telebot.TeleBot(BOT_API)
-
-def load_translation(lang):
-    try:
-        with open(f"./lang/{lang}.json", "r", encoding="utf-8") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print(f"РћС€РёР±РєР°: С„Р°Р№Р» РїРµСЂРµРІРѕРґР° РґР»СЏ СЏР·С‹РєР° {lang} РЅРµ РЅР°Р№РґРµРЅ.")
-        return {}
-
+print(lang_list.get("main_tx"))
 def tr(key):
-    translations = load_translation(lang)
-    return translations.get(key, key)
+    # Получаем шаблон строки по ключу
+    template = lang_list[lang][key]
+
+    if template is None:
+        return f"Шаблон с ключом '{key}' не найден!"
+
+    # Используем eval() для подстановки значений глобальных переменных
+    try:
+        return template.format(**globals())  # Использование globals() для подстановки глобальных переменных
+    except KeyError as e:
+        print(f"Ошибка: переменная {e} не найдена.")
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -40,16 +40,22 @@ def start(message):
         text = tr("main_tx")
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton(text=tr("caog_bt"), callback_data="catalog"))
-        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),InlineKeyboardButton(text=tr("prof_bt"), callback_data="Profile"),InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
+        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),
+                   InlineKeyboardButton(text=tr("prof_bt"), callback_data="Profile"),
+                   InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
 
         bot.send_photo(message.chat.id, img, caption=text, reply_markup=markup)
 
+
 @bot.callback_query_handler(func=lambda call: call.data == "inf")
 def inf(call):
-    with open("./test_2.png","rb") as img:
+    with open("./test_2.png", "rb") as img:
         markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(text=tr("back_bt"),callback_data="main_menu"))
-        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=f"Bot version: {BOT_VERSION}\nMS_ID: {call.message.message_id}"),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=markup)
+        markup.add(InlineKeyboardButton(text=tr("back_bt"), callback_data="main_menu"))
+        bot.edit_message_media(
+            types.InputMediaPhoto(media=img, caption=f"Bot version: {BOT_VERSION}\nMS_ID: {call.message.message_id}"),
+            chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+
 
 @bot.callback_query_handler(func=lambda call: call.data == "main_menu")
 def main_menu(call):
@@ -57,24 +63,40 @@ def main_menu(call):
         text = tr("main_tx")
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton(text=tr("caog_bt"), callback_data="catalog"))
-        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),InlineKeyboardButton(text=tr("prof_bt"), callback_data="profile"),InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
+        markup.row(InlineKeyboardButton(text=tr("info_bt"), callback_data="inf"),
+                   InlineKeyboardButton(text=tr("prof_bt"), callback_data="profile"),
+                   InlineKeyboardButton(text=tr("supp_bt"), callback_data="support"))
 
-        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=f"Р”РѕР±СЂРѕ РїРѕР¶Р°Р»РѕРІР°С‚СЊ РІ РЅР°С€РµРіРѕ С‚РµР»РµРіСЂР°РјРј Р±РѕС‚Р° РґР»СЏ РѕРїР»Р°С‚С‹ Р–РљРҐ \nР­С‚РѕС‚ Р±РѕС‚ РїРѕРјРѕР¶РµС‚ РІР°Рј:\n В· Р№Р№С†С†"), reply_markup=markup,chat_id=call.message.chat.id,message_id=call.message.message_id)
+        bot.edit_message_media(types.InputMediaPhoto(media=img,
+                                                     caption=f"Добро пожаловать в нашего телеграмм бота для оплаты ЖКХ \nЭтот бот поможет вам:\n · ййцц"),
+                               reply_markup=markup, chat_id=call.message.chat.id, message_id=call.message.message_id)
+
 
 @bot.callback_query_handler(func=lambda call: call.data == "support")
 def inf(call):
-    with open("./test_2.png","rb") as img:
+    with open("./test_2.png", "rb") as img:
         markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(text=tr("back_bt"),callback_data="main_menu"))
-        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=tr("supp_tx")),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=markup)
+        markup.add(InlineKeyboardButton(text=tr("back_bt"), callback_data="main_menu"))
+        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=tr("supp_tx")), chat_id=call.message.chat.id,
+                               message_id=call.message.message_id, reply_markup=markup)
 
 
-#РљРѕРЅС‚СЂРѕР»СЊ С‡РµСЂРµР· РєРѕРЅСЃРѕР»СЊ
+@bot.callback_query_handler(func=lambda call: call.data == "profile")
+def inf(call):
+    with open("./test_2.png", "rb") as img:
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text=tr("back_bt"), callback_data="main_menu"))
+        bot.edit_message_media(types.InputMediaPhoto(media=img, caption=tr("prof_tx")), chat_id=call.message.chat.id,
+                               message_id=call.message.message_id, reply_markup=markup)
+
+
+#Контроль через консоль
 async def console_control():
+    bot.send_message(chat_id="5874936084", text="Bot was started")
     while True:
         command = await asyncio.to_thread(input, "> ")
         if command.lower() == "test command":
-            bot.send_message(chat_id="5874936084",text="5874936084")
+            bot.send_message(chat_id="5874936084", text="5874936084")
             print("massage")
         elif command == "stop":
             print("Bot stopped")
@@ -83,11 +105,13 @@ async def console_control():
         else:
             print("unknown command")
 
-#РіР»Р°РІРЅР°СЏ С„СѓРЅРєС†РёСЏ
+
+#главная функция
 async def main():
     bot_task = asyncio.to_thread(bot.infinity_polling)
     console_task = console_control()
     await asyncio.gather(bot_task, console_task)
+
 
 if __name__ == '__main__':
     print("Bot was started")
