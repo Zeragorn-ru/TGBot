deae50040b4a2346a7b0ab518d285132cd2cb6ab
Zeragorn <rosokin52@gmail.com>
2024-12-14 20:14:38 +0300

    releas 1

--- database.py
+++ database.py
@@ -26,10 +26,10 @@ class DataBase:
             self.cursor = self.connect.cursor()
             if self.search_user(message):
                 return
-            self.cursor.execute(f'''INSERT INTO users (user_name, currency, user_id, admin) VALUES (?,?,?,?)''',
-                                (message.from_user.username, "usd", message.from_user.id, 0))
+            self.cursor.execute('''INSERT INTO users (user_name, currency, user_id, admin, orders_count) VALUES (?,?,?,?,?)''',
+                                (message.from_user.username, "usd", message.from_user.id, 0,0))
             self.connect.commit()
-            self.cursor.executecursor.execute(f"""
+            self.cursor.execute(f"""
             CREATE TABLE "{message.from_user.id}" ("id" INTEGER PRIMARY KEY, "event_time" TEXT, "text" TEXT)
             """)
             self.connect.commit()
@@ -69,6 +69,33 @@ class DataBase:
             self.connect.commit()
             return get
 
+    def gufn(self,name):
+        with sqlite3.connect(self.file) as self.connect:
+            self.cursor = self.connect.cursor()
+            self.cursor.execute(f"""
+            SELECT user_id FROM users WHERE user_name = "{name}"
+            """)
+            get = self.cursor.fetchall()
+            self.connect.commit()
+            return get[0][0]
+
+    def add_order(self,message):
+        with sqlite3.connect(self.file) as self.connect:
+            self.cursor = self.connect.cursor()
+            self.cursor.execute(f"SELECT orders_count FROM users WHERE user_id = {message.from_user.id}")
+            get = self.cursor.fetchall()
+            ret = get[0][0] + 1
+            self.cursor.execute(f"UPDATE users SET orders_count = {ret} WHERE user_id = '{message.from_user.id}'")
+            self.connect.commit()
+
+    def get_order(self,message):
+        with sqlite3.connect(self.file) as self.connect:
+            self.cursor = self.connect.cursor()
+            self.cursor.execute(f"SELECT orders_count FROM users WHERE user_id = {message.from_user.id}")
+            get = self.cursor.fetchall()
+            return get[0][0]
+
+
 def add_user(message):
     base = DataBase(BASE_NAME)
     base.add_user(message)
@@ -88,3 +115,15 @@ def rm_remind(user_id, id):
 def get_all_users():
     base = DataBase(BASE_NAME)
     return base.get_all_users()
+
+def gufn(name):#get user id from name
+    base = DataBase(BASE_NAME)
+    return base.gufn(name)
+
+def add_order(message):
+    base = DataBase(BASE_NAME)
+    base.add_order(message)
+
+def get_order(message):
+    base = DataBase(BASE_NAME)
+    return base.gett_order(message)
